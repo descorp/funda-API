404c7ccb77ea6a30a336115d34d9de04543be590
Vladimir Abramichev <vladimir.abramichev@mail.ru>
2019-05-16 21:55:49 +0200

    chore: initiate new project

---
+++ FundaApi/FundaApi/FundaApi.h
@@ -0,0 +1,19 @@
+//
+//  FundaApi.h
+//  FundaApi
+//
+//  Created by Vladimir Abramichev on 16/05/2019.
+//  Copyright © 2019 Vladimir Abramichev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FundaApi.
+FOUNDATION_EXPORT double FundaApiVersionNumber;
+
+//! Project version string for FundaApi.
+FOUNDATION_EXPORT const unsigned char FundaApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FundaApi/PublicHeader.h>
+
+
