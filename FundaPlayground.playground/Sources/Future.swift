dd23d51db710b904b3117d8eaa533041df6d0b22
Vladimir Abramichev <vladimir.abramichev@mail.ru>
2019-05-20 05:06:16 +0200

    chore: add promises

---
+++ FundaPlayground.playground/Sources/Future.swift
@@ -0,0 +1,82 @@
+import Foundation
+import ApiProvider
+
+public class Future<Value> {
+    fileprivate var result: Result<Value>? {
+        // Observe whenever a result is assigned, and report it
+        didSet { result.map(report) }
+    }
+    private lazy var callbacks = [(Result<Value>) -> Void]()
+    public func observe(with callback: @escaping (Result<Value>) -> Void) {
+        callbacks.append(callback)
+        
+        // If a result has already been set, call the callback directly
+        result.map(callback)
+    }
+    private func report(result: Result<Value>) {
+        for callback in callbacks {
+            callback(result)
+        }
+    }
+}
+
+public class Promise<Value>: Future<Value> {
+    public init(value: Value? = nil) {
+        super.init()
+        
+        // If the value was already known at the time the promise
+        // was constructed, we can report the value directly
+        result = value.map(Result.success)
+    }
+     public func resolve(with value: Value) {
+        result = .success(value)
+    }
+    
+    public func reject(with error: Error) {
+        result = .failure(error)
+    }
+}
+
+public extension Future {
+    func than<NextValue>(with closure: @escaping (Value) throws -> Future<NextValue>) -> Future<NextValue> {
+        // Start by constructing a "wrapper" promise that will be
+        // returned from this method
+        let promise = Promise<NextValue>()
+        // Observe the current future
+        observe { result in
+            switch result {
+            case .success(let value):
+                do {
+                    // Attempt to construct a new future given
+                    // the value from the first one
+                    let future = try closure(value)
+                    // Observe the "nested" future, and once it
+                    // completes, resolve/reject the "wrapper" future
+                    future.observe { result in
+                        switch result {
+                        case .success(let value):
+                            promise.resolve(with: value)
+                        case .failure(let error):
+                            promise.reject(with: error)
+                        }
+                    }
+                } catch {
+                    promise.reject(with: error)
+                }
+            case .failure(let error):
+                promise.reject(with: error)
+            }
+        }
+        return promise
+    }
+}
+
+public extension Future {
+    func transform<NextValue>(with closure: @escaping (Value) throws -> NextValue) -> Future<NextValue> {
+        return than { value in
+            return try Promise(value: closure(value))
+        }
+    }
+}
+
+
---
+++ FundaPlayground.playground/Sources/FutureImplementation.swift
@@ -0,0 +1,18 @@
+import Foundation
+import ApiProvider
+
+public extension RemoteApiProvider {
+    func request<T>(_ endpoint: Endpoint<T>) -> Future<T> {
+        let promise = Promise<T>()
+        self.request(endpoint) { response in
+            switch response {
+            case .success(let result):
+                promise.resolve(with: result)
+            case .failure(let error):
+                promise.reject(with: error)
+            }
+        }
+        
+        return promise
+    }
+}
