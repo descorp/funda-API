af27e0012a1b1a5a7e115396b4a08585007fc058
Vladimir Abramichev <vladimir.abramichev@mail.ru>
2019-05-21 21:35:28 +0200

    Create apply_secret.py

---
+++ scripts/apply_secret.py
@@ -0,0 +1,25 @@
+import sys
+import shutil
+import os
+
+# grab repo name
+apiKey = sys.argv[1]
+
+# ios folder path
+libraryPath = os.path.join(sys.path[0] + '/../FundaApi')
+
+def applySecret(secret, basicPath):
+    for path, subdirs, files in os.walk(basicPath):
+        for name in files:
+            with open(os.path.join(path, name), 'r') as file :
+                filedata = file.read()
+
+            # Replace the target string
+            filedata = filedata.replace('API_KEY', secret)
+
+            # Write the file out again
+            with open(os.path.join(path, name), 'w') as file:
+                file.write(filedata)
+
+applySecret(apiKey, libraryPath)
+applySecret(apiKey, appPath)
