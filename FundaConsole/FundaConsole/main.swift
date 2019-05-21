e12d0edb447d166a2ea1073f53941a29cb63dde8
Vladimir Abramichev <vladimir.abramichev@mail.ru>
2019-05-21 18:30:40 +0200

    chore: run assignments in parallel

--- FundaConsole/FundaConsole/main.swift
+++ FundaConsole/FundaConsole/main.swift
@@ -32,57 +32,57 @@ fundaAPI.request(Endpoint.get(type: .koop, search: "amsterdam"))
                     $0.forEach { makelaarStatistic[$0.makelaarNaam] = (makelaarStatistic[$0.makelaarNaam] ?? 0) + 1  }
         })
     }
-    .than { $0.start() }
+    .than { $0.start(verbose: false) }
     .observe { result in
+        defer { dispatchGroup.leave() }
+        
         if case .failure(let error) = result {
             print("Error: \(error)")
             return
         }
         
-        let top10 = makelaarStatistic.sorted(by: { $0.value > $1.value }).prefix(10)
-        
         var position = 1
-        top10.forEach {
+        makelaarStatistic.sorted(by: { $0.value > $1.value }).prefix(10).forEach {
             print("#\(position) \($0.key) - \($0.value)")
             position += 1
         }
-        dispatchGroup.leave()
     }
 
-print("\r = = = = = \r")
-print("Assignmen 2\r")
-dispatchGroup.enter()
-
-makelaarStatistic = [:]
-fundaAPI.request(Endpoint.get(type: .koop, search: "amsterdam", "tuin"))
-    .transform { $0.paging.aantalPaginas }
-    .transform {
-        Reducer(total: $0,
-                nextTry: { pageNumber in
-                    fundaAPI.request(Endpoint.get(type: .koop, size: 25, page: pageNumber, search: "amsterdam", "tuin"))
-                    .transform { $0.objects ?? [] } },
-                onSuccess: {
-                    $0.forEach { makelaarStatistic[$0.makelaarNaam] = (makelaarStatistic[$0.makelaarNaam] ?? 0) + 1  }
-        })
-    }
-    .than { $0.start() }
-    .observe { result in
-        if case .failure(let error) = result {
-            print("Error: \(error)")
-            return
-        }
-        
-        let top10 = makelaarStatistic.sorted(by: { $0.value > $1.value }).prefix(10)
-        
-        var position = 1
-        top10.forEach {
-            print("#\(position) \($0.key) - \($0.value)")
-            position += 1
-        }
-        dispatchGroup.leave()
-}
-
+var quit = false
 dispatchGroup.notify(queue: DispatchQueue.main) {
-    exit(EXIT_SUCCESS)
+    guard !quit else { exit(EXIT_SUCCESS) }
+    quit = true
+    
+    print("\r = = = = = \r")
+    print("Assignmen 2\r")
+    dispatchGroup.enter()
+    
+    makelaarStatistic = [:]
+    fundaAPI.request(Endpoint.get(type: .koop, search: "amsterdam", "tuin"))
+        .transform { $0.paging.aantalPaginas }
+        .transform {
+            Reducer(total: $0,
+                    nextTry: { pageNumber in
+                        fundaAPI.request(Endpoint.get(type: .koop, size: 25, page: pageNumber, search: "amsterdam", "tuin"))
+                            .transform { $0.objects ?? [] } },
+                    onSuccess: {
+                        $0.forEach { makelaarStatistic[$0.makelaarNaam] = (makelaarStatistic[$0.makelaarNaam] ?? 0) + 1  }
+            })
+        }
+        .than { $0.start(verbose: false) }
+        .observe { result in
+            defer { dispatchGroup.leave() }
+            
+            if case .failure(let error) = result {
+                print("Error: \(error)")
+                return
+            }
+
+            var position = 1
+            makelaarStatistic.sorted(by: { $0.value > $1.value }).prefix(10).forEach {
+                print("#\(position) \($0.key) - \($0.value)")
+                position += 1
+            }
+    }
 }
 dispatchMain()
